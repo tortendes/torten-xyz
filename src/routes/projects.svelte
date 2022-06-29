eafefb4c4920bb910dc1a934302ae9b6bf7612d7
Jarelle Emmanuel Payad Andulan <torten@proton.me>
2022-06-30 01:28:31 +0800

    idk what i did

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,12 +1,30 @@
 <h1 class="text-center text-5xl font-black">Projects</h1>
 
-<div class="mt-5 grid grid-cols-2 grid-rows-5">
-  <div class="rounded-lg shadow-2xl">
-    <img src="/hs-static/torten-xyz-promo.png" alt="" width="250" class="w-full">
-    <div class="px-3 py-2 mb-1">
-      <h1 class="text-xl font-bold">torten.xyz</h1>
-      <h3 class="text-md font-medium mb-3">My personal website for blogs and stuff.</h3>
+<div class="grid-group">
+  <div class="card">
+    <img src="/hs-static/torten-xyz-promo.png" alt=""  class="w-full">
+    <div class="text-group">
+      <h1>torten.xyz</h1>
+      <h3>My personal website for blogs and stuff.</h3>
       <span class="text-gray-700">You're here</span>
     </div>
   </div>
 </div>
+
+<style>
+  .grid-group {
+    @apply mt-5 grid grid-cols-2 grid-rows-5
+  }
+  .card {
+    @apply rounded-lg shadow-2xl
+  }
+  .card .text-group {
+    @apply px-3 py-2 mb-1
+  }
+  .card .text-group h1 {
+    @apply text-xl font-bold
+  }
+  .card .text-group h3 {
+    @apply text-base font-medium mb-3
+  }
+</style>
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,26 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+
+test("contact to exist and have text.", async ({ page }) => {
+  await page.goto("/contact");
+  expect(await page.textContent("h1")).toBe("Contact");
+});
+
+test("projects page expected to have the title text", async ({ page }) => {
+  await page.goto("/projects");
+  expect(await page.textContent("h1")).toBe("Projects");
+});
+
+test("projects page expected to have the torten-xyz card.", async ({
+  page,
+}) => {
+  await page.goto("/projects");
+
+  const card = page.locator(".grid-group .card");
+  await expect(card).toBeVisible();
+  const title = page.locator(".grid-group .card h1");
+  await expect(title).toHaveText("torten.xyz");
+
+  const desc = page.locator(".grid-group .card h3");
+  await expect(desc).toHaveText("My personal website for blogs and stuff.");
 });
