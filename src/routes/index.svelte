e3c98c6e0e8b3a73b18c5658e79b98f1cb995a42
Jarelle Emmanuel Payad Andulan <torten@proton.me>
2022-06-29 23:05:34 +0800

    updare

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,29 @@
 <script>
     import PostCard from '../components/PostCard.svelte';
     import Typewriter from 'svelte-typewriter'
+
+    const socials = [
+      {
+	logo: 'Github',
+	link: 'https://github.com/tortendes',
+	iconLocation: '/hs-logo/src/gh.svg'
+      },
+      {
+	logo: 'Twitter',
+	link: 'https://twitter.com/tortenwashere',
+	iconLocation: '/hs-logo/src/twtr.svg'
+      },
+      {
+	logo: 'Twitch',
+	link: 'https://twitch.tv/tortenwashere',
+	iconLocation: '/hs-logo/src/tch.svg'
+      },
+      {
+	logo: 'Email',
+	link: 'mailto:talk@torten.xyz',
+	iconLocation: '/hs-logo/src/ml.svg'
+      },
+    ]
 </script>
 
 
@@ -11,9 +34,10 @@
 <div class="md:flex md:justify-center px-3 md:px-0 overflow-hidden">
     <div class="md:w-1/2 mt-6">
         <div class="flex items-center text-5xl">
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <h1>Hi</h1><img src="https://static.revolt.chat/emoji/mutant/1f44b-10160a.svg" class="w-12 ml-2" alt="Wave emoji">
-            <h1 class="ml-2">I'm</h1><img src="/hs-static/torten-whole-black.webp" class="w-36 ml-3" alt="Logo">
+	    <h1>Hi</h1>
+	    <img src="https://static.revolt.chat/emoji/mutant/1f44b-10160a.svg" class="w-12 ml-2" alt="Wave emoji">
+	    <h1 class="ml-2">I'm</h1>
+	    <img src="/hs-static/torten-whole-black.webp" class="w-36 ml-3" alt="Logo">
         </div>
         <h1 class="mt-2 text-2xl md:text-3xl flex items-center">
             <span class="mr-1">I make</span> <Typewriter loop>
@@ -24,27 +48,14 @@
         </h1>
         <div class="mt-5 flex flex-col items-center justify-center">
             <h1 class="text-3xl font-medium mb-3">Socials</h1>
-            <ul class="flex items-center gap-3">
-                <li>
-                    <a href="https://github.com/tortendes" target="_blank" referrerpolicy="no-referrer">
-                        <img src="/hs-logo/src/gh.svg" alt="GitHub Logo" class="w-10">
-                    </a>
-                </li>
-                <li>
-                    <a href="https://twitch.tv/tortenwashere" target="_blank" referrerpolicy="no-referrer">
-                        <img src="/hs-logo/src/tch.svg" alt="Twitch Logo" class="w-10">
-                    </a>
-                </li>
-                <li>
-                    <a href="https://twitter.com/tortenwashere" target="_blank" referrerpolicy="no-referrer">
-                        <img src="/hs-logo/src/twtr.svg" alt="Twitter Logo" class="w-10">
-                    </a>
-                </li>
-                <li>
-                    <a href="mailto:talk@torten.xyz">
-                        <img src="/hs-logo/src/ml.svg" alt="Email" class="w-10">
-                    </a>
-                </li>
+	    <ul class="flex items-center gap-3">
+	      {#each socials as item}
+		<li>
+		  <a href={item.link}>
+		    <img src={item.iconLocation} alt={`${item.logo} Logo`} class="w-10">
+		  </a>
+		</li>
+	      {/each}
             </ul>
         </div>
         <div>
@@ -52,3 +63,4 @@
         </div>
     </div>
 </div>
+
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,21 @@
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
+import mdsvex from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
   },
+
+  extensions: [".md", ".svelte"],
+
   preprocess: [
     preprocess({
       postcss: true,
+    }),
+    mdsvex({
+      extensions: ['.md']
     }),
   ],
 };
