667d4da73a2864e80401298fdeae7128e590624d
Jarelle Emmanuel Payad Andulan <me@torten.xyz>
2022-12-09 10:19:48 +0800

    fixed colors; added anilist

--- src/components/SocialButton.svelte
+++ src/components/SocialButton.svelte
@@ -3,10 +3,10 @@
     export let text;
     export let link;
 </script>
-<a href={link} target="_blank">
-    <div class="w-full mt-2 border-2 border-black hover:border-torten-yellow py-2 px-10 hover:bg-torten-yellow rounded-md flex justify-center">
+<a href={link} target="_blank" class="min-w-full">
+<div class="border-2 border-black group hover:border-torten-orange py-2 px-10 hover:bg-torten-orange hover:text-white rounded-md flex justify-center">
         <div class="flex gap-2">
-            <img src={icon} alt="Logo" class="hover:fill-white">
+            <img src={icon} alt="Logo" class="group-hover:invert">
             <span>{text}</span>
         </div>
     </div>
--- src/routes/socials.svelte
+++ src/routes/socials.svelte
@@ -7,17 +7,18 @@
     <title>「Socials」 - Torten</title>
 </head>
 
-<div>
+<div class="flex flex-col items-center">
     <div class="flex justify-center">
         <img src="/hs-static/torten-whole-black.webp" alt="Torten Logo" class="w-48">
     </div>
     <div class="mt-5 text-lg">
         <p>Hello! My name is Torten. I make websites as a hobby, here are my social media links!</p>
     </div>
-    <div class="mt-7">
+    <div class="mt-7 flex flex-col items-center gap-2 md:w-1/2">
         <SocialButton icon="hs-logo/src/gh.svg" text="Github" link="https://github.com/tortendes" />
         <SocialButton icon="hs-logo/src/tch.svg" text="Twitch" link="https://twitch.tv/tortenwashere" />
         <SocialButton icon="hs-logo/src/twtr.svg" text="Twitter" link="https://twitter.com/tortenwashere" />
         <SocialButton icon="hs-logo/src/yt.svg" text="YouTube" link="https://youtube.com/@tortendes" />
+        <SocialButton icon="hs-logo/src/al.svg" text="AniList" link="https://anilist.co/user/Torten" />
     </div>
 </div>
